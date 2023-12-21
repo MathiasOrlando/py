2b7fbc2213c6b2fc4983a736e9cc995eddcb83ac
Mi nombre <mthmcl@gmail.com>
2023-12-20 23:06:11 -0300

    second

---
+++ hello.py
@@ -0,0 +1,10 @@
+#HI
+
+print ("hola que tal")
+
+print(type(1)) 
+print(type("hello"))
+print(type(1.7))
+print(type(1+1j))
+
+
---
+++ variables.py
@@ -0,0 +1,14 @@
+#variables
+
+my_variable = "hola de nuevo"
+my_var= 2
+my_vari=False
+my_varia=5+5
+print (my_variable)
+print (my_var)
+print (my_vari)
+print (my_varia)
+
+print (my_variable, my_var, my_vari, my_varia)
+print ("clic", my_variable, "ya", my_var,"veces", my_vari, "con", my_varia)
+
