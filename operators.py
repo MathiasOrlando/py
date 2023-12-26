b3037c88147b4624d508189441c2e63191f253e0
Mi nombre <mthmcl@gmail.com>
2023-12-26 15:13:55 -0300

    commit

--- operators.py
+++ operators.py
@@ -5,3 +5,16 @@ print(3+4)
 print(3-4)
 print(3/4)
 print(3*4)
+print(3//4)
+print(3**4)
+
+print ("hola" * 5)
+my_float= 2.5 *2
+print ("hola " * int(my_float))
+
+print(3>4)
+print(3<4)
+print(3<=4)
+print(3>=4)
+print(3!=4)
+
